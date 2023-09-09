25ae56af588ddf268bfb398deb11ef569aabe36a
Ismael Rangel <ismaelrangel994@gmail.com>
2023-09-09 10:25:12 -0600

    09-09-2023

---
+++ src/ejemplo.ts/01-tipos-basicos.ts
@@ -0,0 +1,18 @@
+
+
+//variables
+
+var num1=1;
+let num2=5;
+const num3=4;
+
+
+let nombre:string='Roberto';
+let num:number=23;
+let activo:boolean=true;
+
+let matricula:number | string | boolean;
+
+matricula=19003410;
+matricula='ywt-2882';
+matricula=false;
---
+++ src/ejemplo.ts/02-obj-intefaz-arre.ts
@@ -0,0 +1,15 @@
+
+
+
+interface Alumnos{
+    matricula?:number;
+    nombre:string;
+    email:string;
+}
+
+const alumnos:Alumnos={
+    nombre: 'juan',
+    email: 'juan@gmail.com',
+}
+
+console.log(alumnos.nombre);
